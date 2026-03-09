da39dc1c62e88bf81acab31fa4e46f57698a3d71
bllacksheep <maclighiche@gmail.com>
2026-03-09 17:28:54 +0000

    test: add init new table test as first test

--- tests/test_ht.c
+++ tests/test_ht.c
@@ -12,6 +12,19 @@ STATIC unsigned item_hash(const char *, const size_t, const unsigned);
 STATIC size_t key_get_len(const char *);
 */
 
+void test_ht_table_init_should_init_a_table_if_not_exist() {
+  ht_table_t *expected = calloc(1, sizeof(ht_table_t));
+  TEST_ASSERT_NOT_NULL_MESSAGE(expected, "failed to allocate a test table");
+
+  ht_table_t *actual = table_init();
+  TEST_ASSERT_NOT_NULL_MESSAGE(actual, "failed to initialize table");
+
+  TEST_ASSERT_EQUAL_MEMORY(expected, actual, sizeof(ht_table_t));
+
+  free(expected);
+  free(actual);
+}
+
 void test_ht_table_get_should_return_an_ht_table(void) {
   ht_table_t *expected = calloc(1, sizeof(ht_table_t));
   TEST_ASSERT_NOT_NULL_MESSAGE(expected, "failed to allocate a test table");
@@ -24,21 +37,19 @@ void test_ht_table_get_should_return_an_ht_table(void) {
   free(expected);
   free(actual);
 }
+
 void test_ht_item_lookup_should_return_an_ht_item(void) {}
 void test_ht_item_hash_should_hash_an_item_key(void) {}
 void test_ht_key_get_len_should_return_the_len_of_an_item_key(void) {}
-void test_ht_table_init_should_initialize_a_table_if_not_exist_else_return_it(
-    void) {}
 
 int main(void) {
   UNITY_BEGIN();
 
+  RUN_TEST(test_ht_table_init_should_init_a_table_if_not_exist);
   RUN_TEST(test_ht_table_get_should_return_an_ht_table);
   RUN_TEST(test_ht_item_lookup_should_return_an_ht_item);
   RUN_TEST(test_ht_item_hash_should_hash_an_item_key);
   RUN_TEST(test_ht_key_get_len_should_return_the_len_of_an_item_key);
-  RUN_TEST(
-      test_ht_table_init_should_initialize_a_table_if_not_exist_else_return_it);
 
   return UNITY_END();
 }
